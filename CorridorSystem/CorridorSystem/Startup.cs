7de4c7fd0ec8c466b5b658c9bab4db81df95b8bb
laje1333 <jesper-larsson@live.com>
2016-04-11 09:59:11 +0200

    A new beginning

---
+++ CorridorSystem/CorridorSystem/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CorridorSystem.Startup))]
+
+namespace CorridorSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
