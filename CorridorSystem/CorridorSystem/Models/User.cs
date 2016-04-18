6b63c98d3985bc369abb4c8cb177025ee6019c3b
viktorbjornlund <viktor.bjornlund@gmail.com>
2016-04-18 10:42:40 +0200

    Created user model and event class

---
+++ CorridorSystem/CorridorSystem/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+using System.ComponentModel.DataAnnotations;namespace CorridorSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int UserType { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Event> Events { get; set; }
+
+        public User() 
+        {
+            Events = new List<Event>();
+        }
+    }
+
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string Status { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int Notify { get; set; }
+        public string Description { get; set; }
+
+    }
+}
