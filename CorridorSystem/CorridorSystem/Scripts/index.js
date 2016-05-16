f96959f2d7f2c6f485e9af3b1b304385308f494f
Linus Rudbeck <rudbeck.linus@gmail.com>
2016-05-16 11:59:40 +0200

    Login-test

---
+++ CorridorSystem/CorridorSystem/Scripts/index.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    $("#btn-login").click(function () { index_event_handlers.on_click_login(); })
+    $("#btn-register").click(function () { index_event_handlers.on_click_register(); })
+});
+
+var index_event_handlers = {
+    on_click_login: function () {
+        var user_name = $("#login-user").val();
+        var password = $("#login-password").val();
+        var data = {
+            UserName: user_name,
+            Password: password
+        };
+
+        console.log(data);
+    },
+
+    on_click_register: function () {
+        var user_name = $("#UserName").val();
+        var password = $("#Password").val();
+        var confirm_password = $("#ConfirmPassword").val();
+        var title = $("#Title").val();
+        var user_type = $("#UserType").val();
+        var email = $("#Email").val();
+        var first_name = $("#FirstName").val();
+        var last_name = $("#LastName").val();
+
+        var data = {
+            UserName: user_name,
+            Password: password,
+            ConfirmPassword: confirm_password,
+            Title: title,
+            Email: email,
+            FirstName: first_name,
+            LastName: last_name
+        };
+
+        console.log(data);
+    }
+};
---
+++ CorridorSystem/CorridorSystem/Scripts/script.js
@@ -0,0 +1,23 @@
+﻿var settings = {
+    api_url: "http://localhost:5998/"
+};
+
+$("btn-login").click(function () {
+    var data = {
+        UserName: $("#login-user").val(),
+        Password: $("#login-password").val()
+    };
+
+    console.log(data);
+
+    $.ajax({
+        url: settings + "api/Token",
+        data: {
+            UserName: $("#login-user").val(),
+            Password: $("#login-password").val(),
+            grant_type: "bearer"
+        }
+    }).done(function (response) {
+        document.cookie = "token="+response.Token;
+    });
+});
