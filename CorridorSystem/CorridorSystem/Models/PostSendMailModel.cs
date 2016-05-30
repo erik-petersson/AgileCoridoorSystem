421affb1a4b4bd424a326d9757baf1211aa19375
Linus Rudbeck <rudbeck.linus@gmail.com>
2016-05-30 12:45:54 +0200

    Api-call för att maila lärare

---
+++ CorridorSystem/CorridorSystem/Models/PostSendMailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CorridorSystem.Models
+{
+    public class PostSendMailModel
+    {
+        public string Subject { get; set; }
+        public string StudentName { get; set; }
+        public string Content { get; set; }
+    }
+}
